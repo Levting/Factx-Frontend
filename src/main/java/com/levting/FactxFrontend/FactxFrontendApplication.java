6f700c498b3b4c6bc212bb47f2c9cf79e550d33a
Sebastián Palate <dpalate2646@uta.edu.ec>
2024-05-21 13:02:56 -0500

    Initial

---
+++ src/main/java/com/levting/FactxFrontend/FactxFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.levting.FactxFrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactxFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactxFrontendApplication.class, args);
+	}
+
+}
