dbfe9c3b303d8b672151bfaf6a554b2790105076
Sebastián Palate <dpalate2646@uta.edu.ec>
2024-06-17 00:47:08 -0500

    Obtencion del PDF

--- src/main/java/com/levting/FactxFrontend/controller/BillingController.java
+++ src/main/java/com/levting/FactxFrontend/controller/BillingController.java
@@ -5,13 +5,29 @@ import com.levting.FactxFrontend.service.BillingService;
 import com.levting.FactxFrontend.service.CustomerService;
 import com.levting.FactxFrontend.service.ProductService;
 import com.levting.FactxFrontend.service.WayPayService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.result.view.RedirectView;
+import org.springframework.web.server.ServerWebExchange;
+
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,11 +35,23 @@ import java.util.Map;
 @RequestMapping("/facturacion")
 public class BillingController {
 
+    // Este controlador se encarga de manejar las peticiones relacionadas con la
+    // facturación de la aplicación
+
+    // Servicios
     private final BillingService billingService;
     private final CustomerService customerService;
     private final ProductService productService;
     private final WayPayService wayPayService;
 
+    /**
+     * Constructor de la clase BillingController (Controlador de la Factura)
+     *
+     * @param billingService
+     * @param customerService
+     * @param productService
+     * @param wayPayService
+     */
     @Autowired
     public BillingController(
             BillingService billingService, CustomerService customerService,
@@ -34,13 +62,32 @@ public class BillingController {
         this.wayPayService = wayPayService;
     }
 
+    /**
+     * Mostrar la página de facturación
+     *
+     * @param model
+     * @return
+     */
     @GetMapping
-    public Mono<String> mostrarPaginaFacturacion(Model model) {
+    public Mono<String> mostrarPaginaFacturacion(Model model, ServerWebExchange serverWebExchange) {
+        // Obtener el usuario del modelo (si esta logueado, si no regresa a iniciar
+        // sesión)
         UserModel usuario = (UserModel) model.getAttribute("usuario");
         if (usuario != null) {
-            return billingService.obtenerFacturas()
-                    .collectList()
-                    .doOnNext(facturas -> {
+
+            return serverWebExchange.getSession()
+                    .flatMap(session -> {
+                        // Añadir los mensajes de éxito o error al modelo
+                        model.addAttribute("successMessage", session.getAttribute("successMessage"));
+                        model.addAttribute("errorMessage", session.getAttribute("errorMessage"));
+
+                        // Limpiar los mensajes de la sesión
+                        session.getAttributes().remove("successMessage");
+                        session.getAttributes().remove("errorMessage");
+
+                        // Obtener las facturas y añadirlas al modelo
+                        return billingService.obtenerFacturas().collectList();
+                    }).doOnNext(facturas -> {
                         if (facturas.isEmpty()) {
                             System.out.println("No existen Facturas!");
                         } else {
@@ -52,7 +99,12 @@ public class BillingController {
         }
     }
 
-    // FORMULARIOS
+    /**
+     * Mostrar el formulario para crear una factura
+     *
+     * @param model
+     * @return
+     */
     @GetMapping("/facturas/crear")
     public Mono<String> mostrarFormularioCrearFactura(Model model) {
         // Añadir un nuevo objeto Factura al modelo
@@ -68,9 +120,18 @@ public class BillingController {
                 .thenReturn("facturacion/crear_factura");
     }
 
+    /**
+     * Abrir una factura
+     *
+     * @param billingModel
+     * @param model
+     * @return
+     */
     @PostMapping("/factura/abrir")
     public Mono<String> abrirFactura(@ModelAttribute("factura") BillingModel billingModel, Model model) {
 
+        // Obtener el id del usuario y el id del cliente del modelo factura al enviar la
+        // petición post
         Integer id_usuario = billingModel.getUsuario().getId_usuario();
         Integer id_cliente = billingModel.getCliente().getId_cliente();
 
@@ -104,22 +165,32 @@ public class BillingController {
                 }).thenReturn("facturacion/crear_factura");
     }
 
+    /**
+     * Añadir un detalle a la factura
+     *
+     * @param detalleFactura
+     * @return
+     */
     @PostMapping("/factura/detalle")
     @ResponseBody
     public Mono<Map<String, Object>> añadirDetalleFactura(@RequestBody Map<String, String> detalleFactura) {
 
+        // Obtener los datos del detalle de la factura
         Integer facturaID = Integer.parseInt(detalleFactura.get("id_factura"));
         Integer productoID = Integer.parseInt(detalleFactura.get("id_producto"));
         Integer cantidad = Integer.parseInt(detalleFactura.get("cantidad"));
 
+        // Añadir el detalle a la factura llamando al servicio de facturación
         return billingService.anadirDetalles(facturaID, productoID, cantidad)
                 .map(detalle -> {
+                    // Crear un mapa de respuesta, en caso de éxito
                     Map<String, Object> response = new HashMap<>();
                     response.put("success", true);
-                    response.put("detalle", detalle);
+                    response.put("detalle", detalle); // Añadir el detalle a la respuesta para obtenerla en AJAX
                     return response;
                 })
                 .onErrorResume(error -> {
+                    // Crear un mapa de respuesta, en caso de error
                     Map<String, Object> response = new HashMap<>();
                     response.put("success", false);
                     response.put("message", "Error al añadir el detalle a la factura: " + error.getMessage());
@@ -127,33 +198,106 @@ public class BillingController {
                 });
     }
 
+    /**
+     * Cerrar Factura
+     *
+     * @param billingModel
+     * @param model
+     * @return
+     */
     @PostMapping("/factura/cerrar")
     public Mono<String> cerrarFactura(
             @ModelAttribute("factura") BillingModel billingModel,
-            Model model) {
+            Model model, ServerWebExchange serverWebExchange) {
+
+        // Obtener el id de la factura y el id de la forma de pago del modelo factura al
+        // enviar la petición post
         int idFactura = billingModel.getIdFactura();
         int idFormaPago = billingModel.getFormaPago().getId_forma_pago();
+
+        // Mostrar por consola la factura que se va a cerrar y su forma de pago
         System.out.println("Factura Cerrada: ID Factura = " + idFactura + ", ID Forma de Pago = " + idFormaPago);
 
         // Cerrar la factura y redicreccionar a la vista de factura
         return billingService.cerrarFactura(idFactura, idFormaPago)
-                .doOnSuccess(success -> System.out.println("Factura Cerrada!"))
-                .doOnError(error -> System.out.println("Error al cerrar la factura: " + error.getMessage()))
-                .thenReturn("redirect:/facturacion");
+                // Añadir los mensajes de éxito a la vista
+                .doOnSuccess(facturaCerrada -> serverWebExchange.getSession()
+                        .doOnNext(session -> session.getAttributes()
+                                .put("successMessage", "Factura Cerrada!"))
+                        .subscribe())
+
+                // Añadir los mensajes de error a la vista
+                .doOnError(error -> {
+                    serverWebExchange.getSession()
+                            .doOnNext(session -> session.getAttributes()
+                                    .put("errorMessage", "Error al cerrar la factura"))
+                            .subscribe();
+                }).thenReturn("redirect:/facturacion");
+        /*
+         * .doOnSuccess(success -> System.out.println("Factura Cerrada!"))
+         * .doOnError(error -> System.out.println("Error al cerrar la factura: " +
+         * error.getMessage()))
+         * .thenReturn("redirect:/facturacion");
+         */
     }
 
+    /**
+     * Mostrar el PDF de la factura
+     *
+     * @param id_factura
+     * @return
+     */
+
+    @Value("${factura.pdf.directory:/uploads/documents/pdf}")
+    private String pdfDirectory;
+
+    @GetMapping("/factura/pdf/{id}")
+    public Mono<ResponseEntity<Object>> mostrarPDF(@PathVariable("id") Integer id_factura) {
+        return billingService.obtenerFactura(id_factura)
+                .flatMap(factura -> {
+                    String claveAcceso = factura.getClave_acceso();
+                    String pdfUrl = "http://localhost:8080/uploads/documents/pdf/" + claveAcceso
+                            + ".pdf";
+
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.setLocation(URI.create(pdfUrl));
+
+                    return Mono.just(new ResponseEntity<>(headers, HttpStatus.FOUND));
+                })
+                .switchIfEmpty(Mono.just(new ResponseEntity<>(HttpStatus.NOT_FOUND)));
+    }
+
+    /**
+     * Buscar clientes por nombre, usado para mostrar los clientes en el buscador
+     *
+     * @param query
+     * @return
+     */
     @GetMapping("/clientes/buscar")
     @ResponseBody
     public Flux<CustomerModel> buscarClientes(@RequestParam("query") String query) {
         return customerService.obtenerClienteNombreOcurrente(query);
     }
 
+    /**
+     * Buscar productos por nombre, usado para mostrar los productos en el buscador
+     *
+     * @param query
+     * @return
+     */
     @GetMapping("/productos/buscar")
     @ResponseBody
     public Flux<ProductModel> buscarProductos(@RequestParam("query") String query) {
         return productService.obtenerProductoNombreOcurrente(query);
     }
 
+    /**
+     * Obtener un producto por su id, usado para contruir el producto en el detalle
+     * para mostar el icono.
+     *
+     * @param id_producto
+     * @return
+     */
     @GetMapping("/productos/{id}")
     @ResponseBody
     public Mono<ProductModel> obtenerProducto(@PathVariable("id") Integer id_producto) {
